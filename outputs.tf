3935348264eb2038d8853bfa695ded032a791f81
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2022-04-17 21:30:38 -0500

    added alb module

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,13 @@
-output "elliott_vpc_id" {
-  value = module.vpc.elliott_vpc_id
-}
 
 # output "elliott_vpc_public_sg" {
 #   value = module.vpc
 # }
+
+
+output "elliott_vpc_id" {
+  value = module.vpc.elliott_vpc_id
+}
+
 
 output "elliott_vpc_public_sg_id" {
   value = module.vpc.elliott_public_sg.id
