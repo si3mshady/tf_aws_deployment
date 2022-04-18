adc447eded3b8fcf5267d181164ed0cc8219b784
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2022-04-17 20:51:42 -0500

    updated outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "elliott_vpc_id" {
+  value = module.vpc.elliott_vpc_id
+}
+
+# output "elliott_vpc_public_sg" {
+#   value = module.vpc
+# }
+
+output "elliott_vpc_public_sg_id" {
+  value = module.vpc.elliott_public_sg.id
+}
+
+output "public_subnets" {
+  value = module.vpc.vpc_public_subnets[*].id
+}
